ca060d8bacd3623c0775f04fbcd4609ede83e73e
dayforjames <sergey.nikitin2013@gmail.com>
2024-03-22 12:39:11 +0300

    Fix bug with MAP func and add first lines of SORT()

--- hw_boolean_search.py
+++ hw_boolean_search.py
@@ -5,62 +5,101 @@ import argparse
 import codecs
 import sys
 import json
+import numpy as np
 
 
 class Index:
     def __init__(self, index_file):
         self._index_file: str = index_file
         self._inverted_index: dict = {}
+        self._line_count = 0
+        self._map_file = './data/MAP_FILE.txt'
+        self._inv_index_file = "./data/inverse_index.json"
 
-    def MAP(self):
+    def process(self):
         with open(self._index_file, "r", encoding="utf-8") as doc:
             for line in doc:
+
                 file_id, file_title, file = line.split("\t")
 
                 file_id = file_id.strip(" \n")
                 file_title = file_title.strip(" \n")
                 file = file_title + " " + file.strip(" \n")
 
-                # with open('./data/MAP_FILE.txt', 'w', encoding='utf-8') as map_file:
-                #     for word in file.split(' '):
-                #         is_valid = True
-
-                #     #!  Maybe should rewrite
-                #         for s in word:
-                #             if not s.isascii():
-                #                 is_valid = False
-                #                 break
-
-                #         if is_valid:
-                #             map_file.write(word + ' ' + file_id + '\n')
-
-                #     map_file.close()
-
                 for word in file.split(" "):
-                    is_valid = True
+                    # is_valid = True
 
+                    #!  Russian word are missed
                     #!  Maybe should rewrite
-                    for s in word:
-                        if not s.isascii():
-                            is_valid = False
-                            break
+                    # for s in word:
+                    #     if not s.isascii():
+                    #         is_valid = False
+                    #         break
 
-                    if is_valid:
-                        if word not in self._inverted_index.keys():
-                            self._inverted_index[word] = set()
+                    # if is_valid:
+                    if word not in self._inverted_index.keys():
+                        self._inverted_index[word] = set()
 
                         self._inverted_index[word].add(int(file_id[1:]))
 
             for key in self._inverted_index.keys():
                 self._inverted_index[key] = list(self._inverted_index[key])
 
-            with open("data/inverse_index.json", "w", encoding="utf-8") as f:
+            with open(self._inv_index_file, "w", encoding="utf-8") as f:
                 json.dump(
                     self._inverted_index,
                 )
 
+    def MAP(self):
+        with open(self._index_file, "r", encoding="utf-8") as doc:
+            with open(self._map_file, "w", encoding="utf-8") as map_file:
+                for line in doc:
+                    file_id, file_title, file = line.split("\t")
+
+                    file_id = file_id.strip(" \n")
+                    file_title = file_title.strip(" \n")
+                    file = file_title + " " + file.strip(" \n")
+                    
+                    for word in file.split(' '):
+                        # is_valid = True
+                        #!  Russian word are missed
+                        # #!  Maybe should rewrite
+                        # for s in word:
+                        #     if not s.isascii():
+                        #         is_valid = False
+                        #         break
+
+                        # if is_valid:
+                        map_file.write(word + ' ' + file_id + '\n')
+
+
+    def _count_file_lines(self, file : str) -> int:
+        count = 0
+
+        with open(file, 'r', encoding='utf-8') as f:
+            for line in f:
+                count += 1
+
+        return count
+
+
     def SORT(self):
-        pass
+        FILE_BUF_COUNT = 50
+
+        self._line_count = self._count_file_lines(self._map_file)
+
+        size = self._line_count // FILE_BUF_COUNT
+        last_size = self._line_count - size * FILE_BUF_COUNT
+
+        line_ind = 0
+
+        size_arr = np.full_like(np.zeros(FILE_BUF_COUNT), size)
+        if last_size != 0:
+            size_arr[-1] = last_size
+
+        print(size_arr)
+
+        # for part_i in range()
 
 
 class QueryTree:
@@ -96,7 +135,9 @@ def main():
 
     # Build index.
     index = Index(args.docs_file)
+    
     index.MAP()
+    index.SORT()
 
     # Process queries.
     search_results = SearchResults()
