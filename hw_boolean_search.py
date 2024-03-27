008927c62237ff15f952febc93994df29144bb60
Sergei Nikitin <nikitin.sergey2015@gmail.com>
2024-03-27 11:48:48 +0300

    modified class QueryTree

--- hw_boolean_search.py
+++ hw_boolean_search.py
@@ -101,12 +101,85 @@ class Index:
 
         # for part_i in range()
 
+def intersection(A : list[int], B : list[int]) -> list[int]:
+    i = j = 0
+    result = []
+
+    A_ = sorted(A)
+    B_ = sorted(B)
+
+    while i < len(A_) and j < len(B_):
+        if A_[i] < B_[j]:
+            i += 1
+        elif A_[i] > B_[j]:
+            j += 1
+        else:
+            result.append(A_[i])
+            i += 1
+            j += 1
+
+    addition = []
+
+    if i < len(A_):
+        addition = A_[i:]
+    elif j < len(B_):
+        addition = B_[j:]
+
+    if len(addition) > 0:
+        for el in addition:
+            if el in A_ and el in B_ and el not in result:
+                result.append(el)
+
+    return result
+
+
+def union(A : list[int], B : list[int]) -> list[int]:
+    i = j = 0
+    result = []
+
+    A_ = A.copy()
+    A_ = sorted(A_)
+
+    B_ = B.copy()
+    B_ = sorted(B_)
+
+    while i < len(A_) and j < len(B_):
+        if A_[i] < B_[j]:
+            result.append(A_[i])
+            i += 1
+        elif A_[i] > B_[j]:
+            result.append(B_[j])
+            j += 1
+        else:
+            result.append(B_[j])
+            i += 1
+            j += 1
+
+    addition = []
+
+    if j < len(B_):
+        addition = B_[j:]
+    elif i < len(A_):
+        addition = A_[i:]
+
+    if len(addition) > 0:
+        for el in addition:
+            if el not in result:
+                result.append(el)
+
+    return result
+
+OPERATORS = {
+    "|": (1, lambda x, y: union(x, y)),
+    " ": (2, lambda x, y: intersection(x, y)),
+}
 
 class QueryTree:
     def __init__(self, qid, query):
         self._query = query
         self._qid = qid
         self._token_list = self.tokenizer(self._query)
+    
 
     def tokenizer(self, request : str) -> list[str]:
         '''Take str request and return list of lexems and operands for next request processing'''
@@ -130,6 +203,44 @@ class QueryTree:
                 i += 1
 
         return token_list
+    
+    def shunting_yard(parsed_formula):
+        stack = []
+        for token in parsed_formula:
+            if token in OPERATORS:
+                while (
+                    stack
+                    and stack[-1] != "("
+                    and OPERATORS[token][0] <= OPERATORS[stack[-1]][0]
+                ):
+                    yield stack.pop()
+                stack.append(token)
+            elif token == ")":
+                while stack:
+                    x = stack.pop()
+                    if x == "(":
+                        break
+                    yield x
+            elif token == "(":
+                stack.append(token)
+            else:
+                yield token
+        while stack:
+            yield stack.pop()
+
+
+    def calc(polish, index):
+        stack = []
+        for token in polish:
+            if token in OPERATORS:
+                y, x = stack.pop(), stack.pop()
+                answ = OPERATORS[token][1](index[x], index[y])
+                index['ANSWER'] = answ
+                stack.append("ANSWER")
+            else:
+                stack.append(token)
+        return index[stack[0]]
+    
 
     def search(self, index):
         # TODO: lookup query terms in the index and implement boolean search logic
